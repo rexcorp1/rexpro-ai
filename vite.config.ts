b0c3ca3cca8262d3ffb872caef0b30b541e9dd5a
rexcorp1 <rexcorp.inc@gmail.com>
2025-09-01 14:35:59 +0700

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,80 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // FIX: Replace `process.cwd()` with `'.'` to resolve a TypeScript error where `cwd` is not found on `process`.
-  // This is functionally equivalent for `loadEnv` in a standard Vite setup.
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.svg', 'logo.svg'],
+        manifest: {
+          name: 'REXPro AI',
+          short_name: 'REXPro AI',
+          icons: [
+            {
+              src: 'icons/icon-48x48.png',
+              sizes: '48x48',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-72x72.png',
+              sizes: '72x72',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-96x96.png',
+              sizes: '96x96',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-128x128.png',
+              sizes: '128x128',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-144x144.png',
+              sizes: '144x144',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-152x152.png',
+              sizes: '152x152',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-256x256.png',
+              sizes: '256x256',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-384x384.png',
+              sizes: '384x384',
+              type: 'image/png'
+            },
+            {
+              src: 'icons/icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ],
+          start_url: '/',
+          display: 'standalone',
+          background_color: '#ffffff',
+          theme_color: '#000000'
+        },
+        devOptions: {
+          enabled: true
+        }
+      })
+    ],
     define: {
       'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY)
     }
